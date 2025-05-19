365bc0d2a2a70e85b57c3b6a9b357dfe65772914
FerrHero <fercaquot@gmail.com>
2025-05-19 11:52:48 +0200

    Update page_acceuil.py

--- page_acceuil.py
+++ page_acceuil.py
@@ -4,14 +4,15 @@ from random import randint
 
 # Interface Tkinter
 fenetre = tk.Tk()
-fenetre.title("Le jeu du motus")
+fenetre.title("Motus Mania")
 fenetre.geometry("540x550")
 fenetre.resizable(False, False)
 
 titre = tk.Label(fenetre, text="Motus Mania", font=("Helvetica", 16))
 titre.pack(pady=10)
 
-
+sous_titre = tk.Label(fenetre, text="trouvez le mot de 7 lettres", font=("Helvetica", 10))
+sous_titre.pack(pady=10)
 
 resultats_frame = tk.Frame(fenetre)
 resultats_frame.pack()
