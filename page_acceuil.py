9e6a13bea859f84ab8f121f9ac94d6c2a127dee0
Sevao <fraigneausevan@gmail.com>
2025-05-19 12:02:29 +0200

    Update page_acceuil.py

--- page_acceuil.py
+++ page_acceuil.py
@@ -1,6 +1,13 @@
+fenetre.mainloop()
 import tkinter as tk
 from tkinter import messagebox
 from random import randint
+
+def joueursolo() :
+	print('efefefe')
+
+def joueurduo() :
+	print('lilkili')
 
 # Interface Tkinter
 fenetre = tk.Tk()
@@ -13,6 +20,12 @@ titre.pack(pady=10)
 
 sous_titre = tk.Label(fenetre, text="trouvez le mot de 7 lettres", font=("Helvetica", 10))
 sous_titre.pack(pady=10)
+
+bouton_valider = tk.Button(fenetre, text="1 Joueur", command=joueursolo, font=("Helvetica", 12))
+bouton_valider.pack(pady=10)
+
+bouton_valider = tk.Button(fenetre, text="2 Joueur", command=joueurduo, font=("Helvetica", 12))
+bouton_valider.pack(pady=10)
 
 resultats_frame = tk.Frame(fenetre)
 resultats_frame.pack()
