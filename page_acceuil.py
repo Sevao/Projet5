3e487eb91f1bc6aa39ea9c2b74aca9fcdebbc202
Sevao <fraigneausevan@gmail.com>
2025-05-19 11:53:24 +0200

    Update page_acceuil.py

--- page_acceuil.py
+++ page_acceuil.py
@@ -8,7 +8,7 @@ fenetre.title("Motus Mania")
 fenetre.geometry("540x550")
 fenetre.resizable(False, False)
 
-titre = tk.Label(fenetre, text="Motus Mania", font=("Helvetica", 16))
+titre = tk.Label(fenetre, text="Motus Mania", font=("Helvetica", 30))
 titre.pack(pady=10)
 
 sous_titre = tk.Label(fenetre, text="trouvez le mot de 7 lettres", font=("Helvetica", 10))
