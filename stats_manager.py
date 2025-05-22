487e24c2257ff542f909b5d2284388dba8521a53
FerrHero <fercaquot@gmail.com>
2025-05-22 23:04:56 +0200

    Update stats_manager.py

--- stats_manager.py
+++ stats_manager.py
@@ -3,22 +3,22 @@ import os
 
 STATS_FILE = "stats.json"
 
-def charger_stats():
+def charger_stats(): #si un fichier statistique existe, celui-ci est récupéré
     if os.path.exists(STATS_FILE):
         with open(STATS_FILE, "r") as f:
             return json.load(f)
-    else:
+    else: # si aucun fichier statistique existe, création d'un nouveau
         return {
             "parties": 0,
             "victoires": 0,
             "total_tours_pour_gagner": 0
         }
 
-def sauvegarder_stats(stats):
+def sauvegarder_stats(stats): # sauvegarde les statistiques d'une partie à l'autre
     with open(STATS_FILE, "w") as f:
         json.dump(stats, f)
 
-def afficher_stats_tk(tk_messagebox):
+def afficher_stats_tk(tk_messagebox): #définition et affichage des statistiques du joueur
     stats = charger_stats()
     parties = stats["parties"]
     victoires = stats["victoires"]
