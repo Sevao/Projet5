bd230c440d377ece81d0dee8177c54623dddbe41
Sevao <fraigneausevan@gmail.com>
2025-05-25 22:34:04 +0200

    Update deux_joueurs.py

--- deux_joueurs.py
+++ deux_joueurs.py
@@ -1,162 +1,70 @@
-#importation des modules
 import tkinter as tk
-from random import choice
+from tkinter import messagebox
 from random import randint
 from stats_manager import *
+from interface import *
+from terminal import *
+from deux_joueurs import *
+import os
 
-# Variables globales
-mot_secret = ""
-mot_7 = []
-joueur = 1
-temps = 15
-timer_id = None
-stats_label_duo = None
+couleur_fond = "#ff9999"  # Rouge-rose clair
 
-def afficher_resultat(mot):
-    ligne = tk.Frame(resultats)
-    ligne.pack(pady=2)
+# Lance le jeu en mode terminal de commande
+def lancer_terminal():
+    fenetre.destroy()
+    mode_terminal()
 
-    etats = ['gris'] * 7
-    temp = list(mot_secret)
+# Lance le jeu en mode interface graphique
+def lancer_interface(nbre_tours):
+    mode_interface(nbre_tours, fenetre, accueil)
 
-    for i in range(7):
-        if mot[i] == mot_secret[i]:
-            etats[i] = 'vert'
-            temp[i] = None
+# Ferme le jeu, après un message de vérification
+def quitter():
+    if messagebox.askokcancel("Quitter", "Voulez-vous vraiment quitter ?"):
+        fenetre.destroy()
 
-    for i in range(7):
-        if etats[i] == 'gris' and mot[i] in temp:
-            etats[i] = 'jaune'
-            temp[temp.index(mot[i])] = None
+# Affiche les différents niveaux disponibles(facile, normal, difficile)
+def niveaux():
+    affichage()
+    tk.Button(fenetre, text="Mode Facile (10 tours de jeu)", command=lambda: lancer_interface(10), font=("Helvetica", 25), bg=couleur_fond).pack(pady=20)
+    tk.Button(fenetre, text="Mode Normal (6 tours de jeu)", command=lambda: lancer_interface(6), font=("Helvetica", 25), bg=couleur_fond).pack(pady=50)
+    tk.Button(fenetre, text="Mode Difficile (4 tours de jeu)", command=lambda: lancer_interface(4), font=("Helvetica", 25), bg=couleur_fond).pack(pady=10)
+    tk.Button(fenetre, text="Retour", command=joueursolo, font=("Helvetica", 15), bg=couleur_fond).pack(pady=10)
 
-    couleurs = {'vert': 'green', 'jaune': 'orange', 'gris': 'gray'}
+# Affiche le choix du jeu solo dans le terminale de commande ou dans l'interface graphique
+def joueursolo():
+    affichage()
+    tk.Button(fenetre, text="Terminal de commande", command=lancer_terminal, font=("Helvetica", 25), bg=couleur_fond).pack(pady=60)
+    tk.Button(fenetre, text="Interface Graphique", command=niveaux, font=("Helvetica", 25), bg=couleur_fond).pack(pady=55)
+    tk.Button(fenetre, text="Retour", command=accueil, font=("Helvetica", 15), bg=couleur_fond).pack(pady=10)
 
-    for i in range(7):
-        tk.Label(ligne, text=mot[i].upper(), bg=couleurs[etats[i]], fg='white',
-                 font=('Arial', 14), width=2).pack(side='left', padx=1)
+# Affiche le choix de l'interface graphique
+def joueurduo():
+    affichage()
+    tk.Button(fenetre, text="Interface Graphique", command=lambda: mode_deux_joueurs(fenetre), font=("Helvetica", 25), bg=couleur_fond).pack(pady=150)
+    tk.Button(fenetre, text="Retour", command=accueil, font=("Helvetica", 15), bg=couleur_fond).pack(pady=10)
 
-def valider():
-    global joueur
-    mot = entree.get().lower()
+# Page d'accueil, avec le choix 1 joueur, ou 2 joueurs
+def accueil():
+    affichage()
+    tk.Button(fenetre, text="1 Joueur", command=joueursolo, font=("Helvetica", 25), bg=couleur_fond).pack(pady=60)
+    tk.Button(fenetre, text="2 Joueurs", command=joueurduo, font=("Helvetica", 25), bg=couleur_fond).pack(pady=55)
+    tk.Button(fenetre, text="Quitter", command=quitter, font=("Helvetica", 15), bg=couleur_fond).pack(pady=10)
 
-    if len(mot) != 7 or not mot.isalpha():
-        message.config(text="Mot invalide. Essayez encore.", fg='red')
-        entree.delete(0, tk.END)
-        return
-
-    arreter_timer()
-    afficher_resultat(mot)
-
-    if mot == mot_secret:
-        message.config(text=f"Joueur {joueur} a gagné !", fg='green')
-
-        # Mise à jour des statistiques
-        stats = charger_stats_duo()
-        stats["parties"] += 1
-        if joueur == 1:
-            stats["victoires_joueur_1"] += 1
-        else:
-            stats["victoires_joueur_2"] += 1
-        sauvegarder_stats_duo(stats)
-        maj_stats_affichage_duo()
-        return
-
-    joueur = 2 if joueur == 1 else 1
-    label_joueur.config(text=f"Joueur {joueur}")
-    changer_couleur_fond()
-    entree.delete(0, tk.END)
-    lancer_timer()
-
-def lancer_timer():
-    global temps, timer_id
-    temps = 15
-
-    def compte():
-        global temps, timer_id
-        temps -= 1
-        label_timer.config(text=f"Temps restant : {temps}s")
-        if temps <= 0:
-            message.config(text=f"Temps écoulé pour le joueur {joueur}", fg='blue')
-            changer_joueur()
-        else:
-            timer_id = fenetre.after(1000, compte)
-
-    timer_id = fenetre.after(1000, compte)
-
-def changer_joueur():
-    global joueur
-    joueur = 2 if joueur == 1 else 1
-    label_joueur.config(text=f"Joueur {joueur}")
-    changer_couleur_fond()
-    entree.delete(0, tk.END)
-    lancer_timer()
-
-def arreter_timer():
-    global timer_id
-    if timer_id:
-        fenetre.after_cancel(timer_id)
-
-def changer_couleur_fond():
-    if joueur == 1:
-        fenetre.config(bg='#add8e6')  # bleu clair
-    else:
-        fenetre.config(bg='#90ee90')  # vert clair
-
-def nouvelle_partie():
-    global mot_secret, joueur
-    arreter_timer()
-    mot_secret = choice(mot_7)
-    joueur = 1
-    label_joueur.config(text="Joueur 1")
-    entree.delete(0, tk.END)
-    for widget in resultats.winfo_children():
-        widget.destroy()
-    message.config(text="")
-    changer_couleur_fond()
-    lancer_timer()
-
-def maj_stats_affichage_duo():
-    try:
-        if stats_label_duo and stats_label_duo.winfo_exists():
-            stats_label_duo.config(text=calculer_affichage_stats_duo())
-    except:
-        pass
-
-def mode_deux_joueurs(f):
-    global fenetre, mot_7, label_joueur, label_timer, entree, resultats, message, stats_label_duo
-    fenetre = f
-
-    with open("liste_mots.txt", "r") as file:
-        mot_7 = [mot.strip().lower() for mot in file if len(mot.strip()) == 7]
-
+# Permet de redefinir les elements de la fenetre
+def affichage():
     for widget in fenetre.winfo_children():
         widget.destroy()
+    fenetre.config(bg=couleur_fond)
+    tk.Label(fenetre, text="Motus Mania", font=("Helvetica", 30), bg=couleur_fond).pack(pady=10)
+    tk.Label(fenetre, text="Trouvez le mot de 7 lettres", font=("Helvetica", 10), bg=couleur_fond).pack(pady=10)
 
-    label_joueur = tk.Label(fenetre, text="Joueur 1", font=("Arial", 16))
-    label_joueur.pack(pady=10)
+# Configuration de la fenêtre principale
+fenetre = tk.Tk()
+fenetre.title("Motus Mania")
+fenetre.geometry("540x550")
+fenetre.iconbitmap(os.path.join(os.path.dirname(__file__), "motus.ico"))
 
-    label_timer = tk.Label(fenetre, text="Temps restant : 15s", font=("Arial", 12))
-    label_timer.pack()
+accueil()
 
-    entree = tk.Entry(fenetre, font=("Arial", 16), justify="center")
-    entree.pack(pady=10)
-    entree.bind("<Return>", lambda event: valider())
-
-    btn_valider = tk.Button(fenetre, text="Valider", font=("Arial", 12), command=valider)
-    btn_valider.pack(pady=5)
-
-    resultats = tk.Frame(fenetre)
-    resultats.pack(pady=10)
-
-    message = tk.Label(fenetre, text="", font=("Arial", 12))
-    message.pack()
-
-    btn_rejouer = tk.Button(fenetre, text="Rejouer", font=("Arial", 12), command=nouvelle_partie)
-    btn_rejouer.pack(pady=10)
-
-    stats_label_duo = tk.Label(fenetre, text="", font=("Arial", 10, "italic"))
-    stats_label_duo.pack(pady=10)
-    maj_stats_affichage_duo()
-
-    changer_couleur_fond()
-    nouvelle_partie()
+fenetre.mainloop()
